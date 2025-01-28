cb767e69453db895ca2b415534224ea8370dc58a
Diego-Escorche <diegoescorche11@gmail.com>
2025-01-28 13:18:03 -0300

    Refactor ls function to use async/await and include file details

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,18 +1,37 @@
 const fs = require('node:fs/promises')
-
+const path = require('node:path')
 const folder = process.argv[2] ?? '.'
 
-fs.readdir(folder)
-    .then(files => {
-        files.forEach(file => {
-            console.log(file)
-        })
-    })
-    .catch(err => {
-        if (err) {
-            console.error('Error al leer el directorio: ', err)
-            return
+async function ls (folder){
+    let files
+    try {
+        files = await fs.readdir(folder)
+    } catch {
+        console.error(`Error al leer el directorio: ${folder}`)
+        process.exit(1)
+    }
+    
+    const filesPromises = files.map(async file => {
+        const filePath = path.join(folder, file)
+        let stats
+
+        try {
+            stats = await fs.stat(filePath) // status - informacion del archivo
+        } catch {
+            console.error(`Error al leer el archivo: ${filePath}`)
+            process.exit(1)
         }
+
+        const isDirectory = stats.isDirectory()
+        const fileType = isDirectory ? 'd' : 'f'
+        const fileSize = stats.size
+        const fileModified = stats.mtime.toLocaleString()
+        
+        return `${fileType} ${file.padEnd(10)} ${fileSize.toString().padStart(10)} ${fileModified}`
+
     })
-// En este caso se usa el metodo readdir para leer un directorio y mostrar los archivos que contiene.
-// En la terminal se pone node 8.ls-advanced.js y el nombre de la carpeta que se quiere leer.
+    const filesInfo = await Promise.all(filesPromises)
+
+    filesInfo.forEach(fileInfo => console.log(fileInfo))
+}
+ls(folder)
